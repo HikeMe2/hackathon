eb4154147e982b765a513ebb871a82a1fad77894
Tal Weiss <Tweiss747@gmail.com>
2022-05-18 15:16:52 +0300

    added interface for user

---
+++ Backend/src/interfaces/IUser.ts
@@ -0,0 +1,14 @@
+//used for ajv
+export interface IUserAJV{
+    email: string,
+    first_name: string,
+    last_name: string,
+    password: string,
+    confirm_password: string
+}
+
+export interface IUser{
+    email: string,
+    first_name: string,
+    last_name: string,
+}
