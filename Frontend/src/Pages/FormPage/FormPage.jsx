56e59e8db454ce97df9abd2fd5a9a0beea5881e8
BenayaCohen <benaya04@gmail.com>
2022-05-18 19:18:02 +0300

    add more q

--- Frontend/src/Pages/FormPage/FormPage.jsx
+++ Frontend/src/Pages/FormPage/FormPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useMemo } from "react";
+import React, { useState, useMemo } from "react";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import Select from "react-select";
 import countryList from "react-select-country-list";
@@ -6,25 +6,30 @@ import Results from "./Results";
 import trails from "../../data/trails.json";
 
 function FormPage() {
-  const [time, setTime] = useState();
-  const [length, setLength] = useState();
-  const [hiking, setHiking] = useState('');
-  const [difficulty, setDifficulty] = useState('');
-  const [country, setCountry] = useState('');
-    const [ results, setResults ] = useState(trails);
-    
-    const options = useMemo(() => countryList().getData(), []);
+  const [time, setTime] = useState("");
+  const [length, setLength] = useState("");
+  const [hiking, setHiking] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [country, setCountry] = useState("");
+  const [climb, setClimb] = useState("");
+  const [maximumAttitude, setMaximumAttitude] = useState("");
+  const [minimumAttitude, setMinimumAttitude] = useState("");
+  const [meters, setMeters] = useState("");
 
-    const changeHandler = (value) => {
-      setCountry(value);
-    };
+  const [results, setResults] = useState(trails);
 
-    function onSearch() {
-      setResults();
-    }
+  const options = useMemo(() => countryList().getData(), []);
+
+  const changeHandler = (value) => {
+    setCountry(value);
+  };
+
+  function onSearch() {
+    setResults();
+  }
 
   return (
-    <Container>
+    <Container  >
       <h1 className="text-center my-3">Find your new hike!</h1>
       <Row className="justify-content-center">
         <Col xs="10" lg="6">
@@ -39,7 +44,9 @@ function FormPage() {
               />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Label>How many kilometers do you want to hike in total?</Form.Label>
+              <Form.Label>
+                How many kilometers do you want to hike in total?
+              </Form.Label>
               <Form.Control
                 type="number"
                 placeholder="Km"
@@ -71,14 +78,55 @@ function FormPage() {
                 <option value="six">6</option>
               </Form.Select>
             </Form.Group>
-            <Form.Group className="mb-5">
+            <Form.Group className="mb-3">
               <Form.Label>Please choose a country</Form.Label>
               <Select
                 options={options}
                 value={country}
                 onChange={changeHandler}
               />
-
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>How many meters do you want to climb?</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="M"
+                value={climb}
+                onChange={(e) => setClimb(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>
+                What is the maximum attitude are you ready to reach?
+              </Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="MAX"
+                value={maximumAttitude}
+                onChange={(e) => setMaximumAttitude(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>
+                What is the minimum attitude are you ready to start from?
+              </Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="MIN"
+                value={minimumAttitude}
+                onChange={(e) => setMinimumAttitude(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>
+                How many meters do you want to climb down?
+              </Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="M"
+                value={meters}
+                onChange={(e) => setMeters(e.target.value)}
+              />
             </Form.Group>
           </Form>
         </Col>
