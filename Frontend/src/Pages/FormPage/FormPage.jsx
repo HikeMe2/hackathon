fb38a3343f58f1e0bab0d268392fdf3804542553
BenayaCohen <benaya04@gmail.com>
2022-05-18 12:30:58 +0300

    welcom and form

--- Frontend/src/Pages/FormPage/FormPage.jsx
+++ Frontend/src/Pages/FormPage/FormPage.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import { Container, Form } from "react-bootstrap";
 
 function FormPage(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Container>
+      <h1 className="mb-5">Find your new hike!</h1>
+      <Form>
+        <Form.Select aria-label="Default select example">
+          <option>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+        <Form.Select aria-label="Default select example">
+          <option>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+        <Form.Select aria-label="Default select example">
+          <option>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+        <Form.Select aria-label="Default select example">
+          <option>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+        <Form.Select aria-label="Default select example">
+          <option>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+        <Form.Select aria-label="Default select example">
+          <option>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+      </Form>
+    </Container>
+  );
 }
 
 export default FormPage;
