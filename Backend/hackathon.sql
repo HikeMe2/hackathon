c160355f197c39e04d46f0e2b39290a3571cfe14
Tal Weiss <Tweiss747@gmail.com>
2022-05-18 18:37:59 +0300

    updated hackathon.sql

--- Backend/hackathon.sql
+++ Backend/hackathon.sql
@@ -9,10 +9,27 @@ CREATE TABLE `user` (
   `password` varchar(50)
 );
 
+
+CREATE TABLE `trail`(
+  `trail_id` varchar(50) PRIMARY KEY,
+  `name` varchar,
+  `length_3d` float,
+  `max_elevation` float,
+  `min_elevation` float,
+  `uphill` float,
+  `downhill` float,
+  `difficulty` varchar,
+  `country` varchar(50),
+  `continent` varchar(50),
+
+)
+
+
 CREATE TABLE `user_images` (
   `img_id` integer PRIMARY KEY,
   `user_id` varchar(50),
   `img_url` varchar(200)
 );
+
 
 ALTER TABLE `user_images` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`email`);
