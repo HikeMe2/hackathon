6ddda1897db09fa098b4336055536f5da56d25fd
zhaojingjing95 <zhaojingjing9595@gmail.com>
2022-05-19 14:45:43 +0300

    form page update

--- Frontend/src/Pages/LoginPage/LoginPage.jsx
+++ Frontend/src/Pages/LoginPage/LoginPage.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import ImageGallery from '../HomePage/ImageGallery';
 import LoginSignupModal from './LoginSignupModal';
 
 function LoginPage() {
     return (
-        <div>
-      <Container style={{ textAlign: "-webkit-center", position: "relative" }}>
-        <h1 className="mb-5">Welcome to Hike Me 2</h1>
-        <p>
-          Hike Me To Travel is a app dedicated to travelers who love to hike.
+      <div>
+        <Container
+          className="home"
+          style={{ textAlign: "-webkit-center", position: "relative" }}
+        >
+          <h1 className="my-2">HikeMe2!</h1>
+          <h5 className="mb-3">
+            {/* Hike Me To Travel is a app dedicated to travelers who love to hike.
           It’s for the travelers who strive to integrate the great outdoors into
           their travel plans. This website brings you the best hiking trails in
           the world, Whether you’re a day hiker or long-distance trekker, you
           can use our in-depth guides and itineraries to plan your next
-          adventure.
-        </p>
-      </Container>
-      <LoginSignupModal/>
-    </div>
+          adventure. */}
+            Bringing Hikes Closer To You
+          </h5>
+          <ImageGallery />
+          {/* <ImageSlides/> */}
+        </Container>
+        <LoginSignupModal />
+      </div>
     );
 }
 
