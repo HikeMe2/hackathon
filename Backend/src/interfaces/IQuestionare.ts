f2101d2c9f8061bcb27100eee6b8e320866dd64e
Tal Weiss <Tweiss747@gmail.com>
2022-05-19 10:57:25 +0300

    fixed interface

--- Backend/src/interfaces/IQuestionare.ts
+++ Backend/src/interfaces/IQuestionare.ts
@@ -1,7 +1,23 @@
 export interface IQuestionare{
-    time: number,
-    length: number,
-    hiking: string,
-    location: string,
-    difficulty: string
+    // "length_3d": 10,
+    // "max_elevation": 1000,
+    // "uphill": 300,
+    // "moving_time": 2,
+    // "difficulty": 3,
+    // "min_elevation": 0,
+    // "downhill": 200,
+    // "country": "Austria",
+    // "Hiking Environment": "Mountain"
+    lenght_3d:number,
+    max_elevation: number,
+    uphill: number,
+    moving_time: number,
+    difficulty: number,
+    min_elevation: number,
+    downhill: number,
+    country: string,
+    hiking_environment: string
+
+
+
 }
